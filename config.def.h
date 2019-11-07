984de4f7a6946732892b34a19115f13efa88d8b3
kaarks <kaarx@users.noreply.github.com>
2019-11-07 12:21:55 +1100

    Changed font to Hack and setup gruvbox colours

--- config.def.h
+++ config.def.h
@@ -1,21 +1,38 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "Hack Nerd Font:size=14" };
+static const char dmenufont[]       = "Hack Nerd Font:size=14";
+/*
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_sel_bg[]      = "#fb4934";*/
+
+/* gruvbox */
+static const char col_bg0_h[]  = "#1d2021";
+static const char col_bg0[]    = "#282828";
+static const char col_bg1[]    = "#3c3836";
+static const char col_bg2[]    = "#504945";
+static const char col_bg3[]    = "#665c54";
+static const char col_bg4[]    = "#7c6f64";
+static const char col_fg0[]    = "#fbf1c7";
+static const char col_fg1[]    = "#ebdbb2";
+static const char col_fg2[]    = "#d5sva1";
+static const char col_fg3[]    = "#bdae93";
+static const char col_fg4[]    = "#a89984";
+static const char col_red[]    = "#fb4934";
+
+
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_fg0, col_bg0, col_bg0 },
+	[SchemeSel]  = { col_bg0_h, col_red,  col_red  },
 };
 
 /* tagging */
@@ -56,7 +73,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_bg1, "-nf", col_fg0, "-sb", col_red, "-sf", col_bg0_h, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
