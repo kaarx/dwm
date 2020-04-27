a711d6541873dbc051ad068f06c00c5106c1dc95
kaarx <kaarx@users.noreply.github.com>
2020-04-27 12:26:59 +1000

    Changed font to Fira Code

--- config.def.h
+++ config.def.h
@@ -2,12 +2,12 @@
 
 /* appearance */
 static const unsigned int borderpx  = 5;        /* border pixel of windows */
-static const unsigned int gappx     = 5;        /* gaps between windows */
+static const unsigned int gappx     = 10;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Hack Nerd Font:size=14" };
-static const char dmenufont[]       = "Hack Nerd Font:size=16";
+static const char *fonts[]          = { "Fira Code Retina:size=14" };
+static const char dmenufont[]       = "Fira Code Retina:size=16";
 /*
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
@@ -31,7 +31,7 @@ static const char col_red[]    = "#fb4934";
 static const char col_purple[] = "#b16286";
 static const char col_orange[] = "#d65d0e";
 
-static const unsigned int baralpha = 0xd0;
+static const unsigned int baralpha = 0xa0;
 static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
